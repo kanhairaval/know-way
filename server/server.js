40d9073cb67c9e7725e67257fad78e2f165013f2
Kanhai Raval <kanhairaval@Kanhais-MacBook-Pro-2.local>
2023-10-21 11:26:39 -0400

    created initial folder structure and installed apollo, express & mongoose

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const db = require('./config/connection');
 
 const app = express();
+const Port = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
