0a32bf790344bb22b491dfee52d3e840f2094a0b
Kanhai Raval <kanhairaval@Kanhais-MacBook-Pro-2.local>
2023-10-24 19:00:58 -0400

    connection.js code with mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose');
 
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/knowway',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+  });
+
+module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const app = express();
-const Port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
