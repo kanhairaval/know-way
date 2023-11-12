64933193ae9199ca61397dd2bc247f135a6ac7aa
Kanhai Raval <kanhairaval@gmail.com>
2023-11-11 19:54:29 -0500

    fixed reposnsive of create article page and made little progress towards view article page

--- client/src/components/ArticleView.js
+++ client/src/components/ArticleView.js
@@ -4,66 +4,53 @@ import "../style/ArticleView.css";
 const ArticleView = () => {
     return (
         <section className = "view-article">
-
-            <div className = "article-header">
-                    <div className="article-details">
-                        <h2 id = "article-title">Will AI lead to terminator?</h2>
-                        <h4>Fact or opinion</h4>
-                        <div className = "details">
-                        <h4>Technology</h4> <h4>KanhaiRaval</h4> <h4>05/13/2042</h4>
+            <div className="master-header-div">
+                <div className = "article-header-div">
+                    <div className = "article-header-sub-div">
+                        <h2 class = "title-details" id = "title">Lionel Messi: Beyond Football Greatness, the Epitome of Athletic Excellence</h2>
+                        <h4 class = "title-details" id = "fact-opinion">Opinion</h4>
+                    </div>    
+                    <div className = "article-header-sub-div-2">
+                        <h4>Sports</h4> <h4>KanhaiRaval</h4> <h4>05/13/2042</h4>
                     </div>
                 </div>
 
                 <div className = "article-image-div">
-                    <img id = "article-image" alt = "article-image" src = "ai-terminator.jpeg"/>
+                    <img id = "article-image" alt = "article-image" src = "messi-world-cup.jpeg"/>
                 </div>
             </div>
 
-            <p id = "article-content">The idea of artificial intelligence (AI) leading to a dystopian future, akin to the Terminator movies, has been a recurring theme in popular culture and discussions about technology. However, it is my personal opinion that AI will not lead us down such a catastrophic path. While there are legitimate concerns about the ethical and safety aspects of AI, it is crucial to dispel the notion that we are on the verge of a robot apocalypse. In this article, I will explain why I believe that AI's potential dangers are manageable, and there are strong reasons to be optimistic about the future of AI.
+            <p id = "article-content">TIn the realm of sports, few figures can transcend their respective disciplines to become synonymous with greatness. Lionel Messi, recently crowned with his record-breaking eighth Ballon d'Or, not only stands as the greatest football player of all time but also as the unparalleled epitome of athletic brilliance.
 
-AI's Current State
+Dominance Beyond Numbers:
 
-AI has made remarkable advancements in recent years, but it is essential to understand the current state of AI technology. Most AI systems today are narrow or weak AI, designed to perform specific tasks or solve particular problems. These systems are highly specialized and lack the generalized intelligence, consciousness, and self-awareness portrayed in the Terminator movies. AI can beat humans at chess, recognize speech, and even assist in medical diagnosis, but these capabilities do not imply any intent or desire for world domination.
+Messi's statistical achievements are staggering, with records shattered and milestones conquered. Yet, reducing his greatness to numbers alone fails to capture the essence of his unparalleled contribution to the beautiful game. Messi's dominance extends far beyond the scorecards; it lies in the ethereal quality of his play, the mastery of the ball, and the sheer joy he brings to the field.
 
-Ethical and Safety Concerns
+Magical Dribbling and Playmaking:
 
-My opinion aligns with those who acknowledge that there are genuine ethical and safety concerns associated with AI. As AI becomes increasingly integrated into our daily lives, it is essential to address these issues proactively. Ensuring that AI technologies are developed and deployed responsibly is a collective responsibility. Companies, governments, and researchers must adhere to ethical guidelines and safety measures, as they strive to harness AI's potential.
+Messi's style of play is a masterclass in finesse, balance, and innovation. His low center of gravity, combined with an otherworldly ball control, allows him to navigate through defenses with a grace that transcends the sport. His dribbling is not just a means to an end; it's a symphony of movement that mesmerizes spectators and disheartens opponents.
 
-One of the primary concerns is the misuse of AI for harmful purposes. This includes using AI in cyberattacks, spreading disinformation, or even developing autonomous weapons. While these are valid concerns, it is not the technology itself but the intentions and actions of individuals and organizations that pose a threat. By implementing robust regulations, international agreements, and accountability measures, we can mitigate these risks.
+His playmaking abilities are equally extraordinary. Messi has an innate sense of the game, an ability to orchestrate attacks, and a vision that transforms football into an art form. His assists are as breathtaking as his goals, creating a unique legacy as a player who is not just a scorer but a creator.
 
-Another concern is the potential for AI to reinforce and perpetuate biases. AI systems are trained on vast datasets, and if these datasets contain biases, the AI can inadvertently perpetuate those biases. It is crucial to address this issue through improved data collection, diverse and representative datasets, and the development of algorithms that can detect and correct biases.
+Consistency and Longevity:
 
-The Human Element
+What sets Messi apart from his peers and predecessors is not just his moments of brilliance but the sustained excellence over the span of his illustrious career. From his early days at Barcelona's La Masia to his current tenure at Paris Saint-Germain, Messi's consistency remains a testament to his unwavering commitment to perfection.
 
-In my opinion, one of the strongest reasons AI will not lead to a Terminator-like future is the continued importance of human oversight and decision-making. While AI can process vast amounts of data and perform tasks at incredible speeds, it lacks the ability to make moral or ethical judgments. It is humans who design AI systems, set their objectives, and define their values. As long as human beings remain in control, AI will not evolve independently and make catastrophic decisions.
+Many athletes experience peaks, but Messi's ascent seems perpetual. Season after season, he defies the conventional wisdom of athletic decline, continually evolving his game to stay ahead of the competition. This relentless pursuit of excellence cements his status as not just a football legend but an athletic phenomenon.
 
-Furthermore, AI is a tool that extends human capabilities rather than replaces them. It can assist doctors in diagnosing diseases, help scientists analyze data, and automate repetitive tasks, but it does so under human supervision. The synergy between humans and AI is what will drive progress and innovation in various fields.
+Global Impact:
 
-The Need for Education and Awareness
+Messi's influence extends far beyond the pitch. His global impact is not solely confined to football; it transcends cultural, geographical, and generational boundaries. Messi is more than an athlete; he is a symbol of inspiration for aspiring talents and a source of joy for millions of fans worldwide.
 
-To ensure AI's safe and responsible development, my opinion is that education and awareness are crucial. It is essential for society as a whole to understand AI and its implications. This includes educating policymakers, business leaders, and the general public about AI's potential and limitations. With a better understanding of AI, we can make informed decisions and develop the necessary safeguards.
+His humility and sportsmanship further amplify his influence, making him a role model for aspiring athletes who seek not just success but a genuine connection with the sport and its fans.
 
-AI literacy should be integrated into education curricula at various levels, empowering the younger generation to engage with AI responsibly. Additionally, public awareness campaigns and initiatives can help dispel misconceptions about AI, reducing the fear of a Terminator-like scenario.
+G.O.A.T of All Sports:
 
-AI Governance and Collaboration
+While debates about the greatest athlete of all time often involve athletes from various disciplines, Messi's case stands out. His transcendent skill set, combined with an enduring commitment to excellence, makes him a contender not just for the greatest footballer but for the greatest athlete in the history of sport.
 
-Another reason for optimism in my opinion is the increasing focus on AI governance and international collaboration. Many governments and organizations are actively working on establishing ethical guidelines, regulations, and frameworks for AI development and deployment. Initiatives like the Partnership on AI and the AI Ethics Guidelines by the European Commission exemplify the commitment to ensuring that AI benefits society.
+Athletic greatness is not solely measured by physical prowess; it encompasses mental fortitude, leadership, and an enduring impact on the sport's cultural fabric. Messi, with his unmatched skill, leadership on and off the field, and a legacy that extends beyond the boundaries of football, encapsulates the essence of what it means to be the greatest athlete of all time.
 
-Collaboration between different stakeholders, including governments, academia, industry, and civil society, can help address AI's challenges collectively. By sharing knowledge and resources, we can create a global ecosystem that promotes responsible AI development.
-
-AI's Beneficial Impact
-
-It is essential to highlight the many ways AI is already making a positive impact on society. In healthcare, AI aids in early disease detection and the development of personalized treatment plans. In agriculture, AI optimizes crop yields and reduces environmental impact. AI-powered transportation systems can enhance road safety and reduce traffic congestion. These are just a few examples of how AI can improve our lives.
-
-My opinion is that focusing on AI's potential to solve real-world problems, rather than dwelling on dystopian scenarios, can lead to a more constructive and optimistic outlook. AI has the potential to revolutionize various industries, create new job opportunities, and improve overall quality of life.
-
-Conclusion
-
-In conclusion, my personal opinion is that AI will not lead to a Terminator-like future. While there are legitimate concerns regarding AI's ethical and safety aspects, they are not insurmountable. Responsible development, robust regulations, and human oversight are key factors in ensuring AI remains a tool that benefits humanity.
-
-Education and awareness are essential to demystify AI and empower individuals and society to engage with it responsibly. The ongoing efforts to establish AI governance and international collaboration demonstrate a commitment to ethical and safe AI development. AI's beneficial impact on various fields is already evident, and its potential to address real-world problems is a reason for optimism.
-
-As we move forward with AI technology, it is crucial to maintain a balanced perspective and focus on harnessing its potential for the betterment of society, rather than dwelling on doomsday scenarios. By doing so, we can ensure that AI remains a force for good and does not lead us down the path of the Terminator.</p>
+In the end, the debate about Lionel Messi's place in the pantheon of sports legends is not merely a matter of opinion; it's an acknowledgment of a transcendent talent whose impact reverberates far beyond the football pitch. Messi's eighth Ballon d'Or is not just a record; it's a coronation of a sporting deity whose influence will be felt for generations to come.</p>
 
         </section>
     );
